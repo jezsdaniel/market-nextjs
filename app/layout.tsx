7b054e608651286a03d230c954320f4ca5fc3fbb
jezsdaniel <jadendaniel7@gmail.com>
2023-05-29 15:05:02 -0400

    Fist commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
