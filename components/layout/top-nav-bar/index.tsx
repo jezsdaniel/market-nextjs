173502c6f10c381d04744b361469f08c2496dc1c
jezsdaniel <jadendaniel7@gmail.com>
2023-05-31 17:26:14 -0400

    Add nav bar bottom

--- components/layout/top-nav-bar/index.tsx
+++ components/layout/top-nav-bar/index.tsx
@@ -7,19 +7,23 @@ import { FressLogoText } from '@/components/icons';
 import { LocationSelector } from './location-selector';
 import { SearchInput } from './search-input';
 import { NavBarActions } from './nav-bar-actions';
+import { NavBarBottom } from './nav-bar-bottom';
 
 export const TopNavBar = () => {
   return (
     <>
-      <nav className="hidden lg:flex flex-row items-center justify-between max-w-7xl w-full p-6 space-x-6">
-        <Link href="/" aria-label="Inicio">
-          <FressLogoText className="h-10" />
-        </Link>
-        <LocationSelector />
-        <SearchInput />
-        <NavBarActions />
+      <nav className="hidden lg:flex flex-col max-w-7xl w-full p-6 space-y-4">
+        <div className="flex flex-row items-center justify-between space-x-6">
+          <Link href="/" aria-label="Inicio">
+            <FressLogoText className="h-10" />
+          </Link>
+          <LocationSelector />
+          <SearchInput />
+          <NavBarActions />
+        </div>
+        <NavBarBottom />
       </nav>
-      <nav className="flex lg:hidden flex-col max-w-7xl w-full p-4 space-y-4">
+      <nav className="flex lg:hidden flex-col w-full p-4 space-y-4">
         <div className="flex flex-row items-center justify-between space-x-6">
           <button
             type="button"
