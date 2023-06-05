d7a2575e014f6947a1393e83d13ae44eff8ec43a
jezsdaniel <jadendaniel7@gmail.com>
2023-06-05 04:44:37 -0400

    Add fress, new and clean products carousels

--- features/store/ui/products-carousel.tsx
+++ features/store/ui/products-carousel.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import { UilCornerUpRightAlt } from '@iconscout/react-unicons';
 
-import sampleFood1 from 'public/img/samples/sample-food-1.png';
+import { IProductMock } from '../models/i-product-mock';
 
-export const ProductsCarousel = () => {
+interface ProductsCarouselProps {
+  title: string;
+  products: IProductMock[];
+}
+
+export const ProductsCarousel: React.FC<ProductsCarouselProps> = ({
+  title,
+  products,
+}) => {
   return (
-    <div className="flex flex-col w-full py-8">
+    <div className="flex flex-col w-full">
       <div className="flex flex-row w-full items-center justify-between space-x-4 px-4 md:px-16 lg:px-24 xl:px-36">
-        <h6 className="text-2xl md:text-3xl font-bold">🤩 Nuevas ofertas</h6>
+        <h6 className="text-2xl md:text-3xl font-bold">{title}</h6>
         <div className="hidden lg:flex flex-row items-center space-x-4">
           <button
             disabled
@@ -29,39 +37,29 @@ export const ProductsCarousel = () => {
           </button>
         </div>
       </div>
-      <div className="flex flex-row w-full overflow-x-auto space-x-1 px-4 md:px-12 lg:px-16 xl:px-24 py-8">
-        <ProductsCarouselItem isNew image={sampleFood1} price={179} discountPrice={179} />
-        <ProductsCarouselItem isNew image={sampleFood1} price={179} discountPrice={179} />
-        <ProductsCarouselItem isNew image={sampleFood1} price={179} discountPrice={179} />
-        <ProductsCarouselItem isNew image={sampleFood1} price={179} discountPrice={179} />
-        <ProductsCarouselItem isNew image={sampleFood1} price={179} discountPrice={179} />
+      <div className="flex flex-row w-full overflow-x-auto space-x-1 px-4 md:px-12 lg:px-16 xl:px-24 pt-8 pb-16">
+        {products.map((product, index) => (
+          <ProductsCarouselItem key={index} product={product} />
+        ))}
       </div>
     </div>
   );
 };
 
 interface ProductsCarouselItemProps {
-  isNew?: boolean;
-  image: string | StaticImageData;
-  price: number;
-  discountPrice?: number;
+  product: IProductMock;
 }
 
-const ProductsCarouselItem: React.FC<ProductsCarouselItemProps> = ({
-  isNew = false,
-  image,
-  price,
-  discountPrice,
-}) => {
+const ProductsCarouselItem: React.FC<ProductsCarouselItemProps> = ({ product }) => {
   return (
     <div className="relative flex flex-col min-w-[272px] w-[272px] rounded-2xl hover:bg-gray-50 hover:drop-shadow-xl p-6">
-      {isNew && (
+      {product.isNew && (
         <div className="absolute flex items-center justify-center text-[11px] leading-3 font-semibold text-neutral-50 bg-amber rounded-full top-3 right-3 px-2 py-1">
           nuevo
         </div>
       )}
       <Image
-        src={image}
+        src={product.image}
         alt="Combo 1"
         placeholder="blur"
         style={{
@@ -73,13 +71,16 @@ const ProductsCarouselItem: React.FC<ProductsCarouselItemProps> = ({
         }}
       />
       <div className="flex row items-baseline space-x-3 mt-4">
-        {discountPrice && (
+        {product.discountPrice && (
           <span className="text-base font-medium text-neutral-500 line-through">
-            ${price.toFixed(2)}
+            ${product.price.toFixed(2)}
           </span>
         )}
         <span className="text-lg font-bold">
-          ${discountPrice ? discountPrice.toFixed(2) : price.toFixed(2)}
+          $
+          {product.discountPrice
+            ? product.discountPrice.toFixed(2)
+            : product.price.toFixed(2)}
         </span>
       </div>
       <p className="text-base leading-5 font-medium line-clamp-2 mt-2">
