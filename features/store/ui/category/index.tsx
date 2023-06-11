d0ee567ea4edb4d52830298f8e7befdaa1940608
jezsdaniel <jadendaniel7@gmail.com>
2023-06-11 01:28:16 -0400

    Add carousel to category page

--- features/store/ui/category/index.tsx
+++ features/store/ui/category/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import { Breadcrums } from '@/components/breadcrums';
+import { StoreSecondBanner } from '../../components/banners';
+import { ProductsCarousel } from '../../components/carousel';
+
+import sampleFood1 from 'public/img/samples/sample-food-1.png';
+import sampleFood2 from 'public/img/samples/sample-food-2.png';
+import sampleFood3 from 'public/img/samples/sample-food-3.png';
+import sampleFood4 from 'public/img/samples/sample-food-4.png';
+import sampleFood5 from 'public/img/samples/sample-food-5.png';
 
 export const CategoryUI = () => {
   return (
@@ -15,6 +23,39 @@ export const CategoryUI = () => {
           {
             name: 'Productos',
             href: '/',
+          },
+        ]}
+      />
+      <StoreSecondBanner />
+      <ProductsCarousel
+        title="🤩 Nuevas ofertas"
+        products={[
+          {
+            isNew: true,
+            image: sampleFood2,
+            price: 179,
+            discountPrice: 149,
+          },
+          {
+            isNew: true,
+            image: sampleFood1,
+            price: 179,
+            discountPrice: 149,
+          },
+          {
+            isNew: true,
+            image: sampleFood3,
+            price: 179,
+          },
+          {
+            isNew: true,
+            image: sampleFood4,
+            price: 179,
+          },
+          {
+            isNew: true,
+            image: sampleFood5,
+            price: 179,
           },
         ]}
       />
