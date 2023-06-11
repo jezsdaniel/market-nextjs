5e3a526e5ac3160393111f9e8121477c29fabbbc
jezsdaniel <jadendaniel7@gmail.com>
2023-06-11 18:59:06 -0400

    Add products container header desktop

--- features/store/ui/category/products-list-container/header.tsx
+++ features/store/ui/category/products-list-container/header.tsx
@@ -9,11 +9,11 @@ import { FilterChip } from './filter-chip';
 
 export const ProductsListHeader = () => {
   return (
-    <div className="flex flex-col w-full items-start">
+    <div className="flex flex-col lg:flex-row w-full items-start lg:items-center lg:justify-between">
       <h6 className="text-sm font-medium text-neutral-500">200 productos encontrados</h6>
-      <div className="mt-6 flex flex-row w-full space-x-4">
+      <div className="mt-6 lg:mt-0 flex flex-row w-full lg:w-auto space-x-4">
         <SelectButton
-          className="grow"
+          className="grow lg:grow-0"
           buttonClassName="h-10 justify-between text-sm"
           items={[
             {
@@ -27,9 +27,9 @@ export const ProductsListHeader = () => {
           }}
           onChange={(value) => {}}
         />
-        <IconButton icon={<UilFilter size={24} />} />
+        <IconButton className="lg:hidden" icon={<UilFilter size={24} />} />
       </div>
-      <div className="flex flex-row flex-wrap">
+      <div className="flex lg:hidden flex-row flex-wrap">
         <FilterChip text="Categoría" className="mt-2 mr-1" />
         <FilterChip text="$0 - $300" className="mt-2 mr-1" />
       </div>
